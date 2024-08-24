574754131c979269430ddab762e2db350cde8a2b
Zambeli <123279272+Zambeli@users.noreply.github.com>
2024-08-24 16:09:42 +0000

    Pedindo um acai junto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Olá mãe, so queria lhe dizer q t amo e pedir 2 reais emprestado xD 💕</h1>
+    return <h1>Olá mãe, so queria lhe dizer q t amo e pedir 20 reais emprestado ou comprar um açai para mim xD 💕</h1>
 }
 
 export default Home;
