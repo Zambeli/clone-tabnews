1f6b18e2933d54ee55a1a45afa4d61bab6a5adcf
Zambeli <123279272+Zambeli@users.noreply.github.com>
2024-08-26 20:31:31 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto baseado no tabnews.com.br criado junto com Filipe Deschamps no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home () {
-    return <h1>Olá mãe, so queria lhe dizer q t amo e pedir 20 reais emprestado ou comprar um açai para mim xD 💕</h1>
+function Home() {
+  return (
+    <h1>
+      Olá mãe, so queria lhe dizer q t amo e pedir 20 reais emprestado ou
+      comprar um açai para mim xD 💕
+    </h1>
+  );
+}
+
+function teste() {
+  console.log('teste');
+}
+
+function teste() {
+  console.log('dhuwauhidaw')
 }
 
 export default Home;
