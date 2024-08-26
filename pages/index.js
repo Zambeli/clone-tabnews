90058b9dff0e50688117033779c30459d13ed67c
Zambeli <123279272+Zambeli@users.noreply.github.com>
2024-08-26 20:41:08 +0000

    colocando . no final

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,9 @@ function Home() {
   return (
     <h1>
       Olá mãe, so queria lhe dizer q t amo e pedir 20 reais emprestado ou
-      comprar um açai para mim xD 💕
+      comprar um açai para mim xD. 💕
     </h1>
   );
-}
-
-function teste() {
-  console.log('teste');
-}
-
-function teste() {
-  console.log('dhuwauhidaw')
 }
 
 export default Home;
