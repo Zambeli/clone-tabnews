ef24ec30a386be8ccd344f5d8abb29854851a161
Gustavo Zambeli <123279272+Zambeli@users.noreply.github.com>
2025-07-02 13:45:03 +0000

    fix ´/migrations´ endpoint conection bug with delete test

---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,7 @@
+test("DELETE to /api/v1/migrations should return 405", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+
+  expect(response.status).toBe(405);
+});
