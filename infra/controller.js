12a808a380fc31c691038d8adc17cbf7146a795f
Gustavo Zambeli <123279272+Zambeli@users.noreply.github.com>
2025-10-20 14:02:23 +0000

    feat: implement session creation

--- infra/controller.js
+++ infra/controller.js
@@ -3,6 +3,7 @@ import {
   InternalServerError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors";
 
 function onNoMatchHandler(request, response) {
@@ -12,12 +13,15 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (error instanceof ValidationError || error instanceof NotFoundError) {
+  if (
+    error instanceof ValidationError ||
+    error instanceof NotFoundError ||
+    error instanceof UnauthorizedError
+  ) {
     return response.status(error.statusCode).json(error);
   }
 
   const publicErrorObject = new InternalServerError({
-    statusCode: error.statusCode,
     cause: error,
   });
 
