8f1d2fc835c26d8eb56b389eb900ec80c3a3ee44
Gustavo Zambeli <123279272+Zambeli@users.noreply.github.com>
2025-10-23 16:23:03 +0000

    feat: implement session deletion

--- models/session.js
+++ models/session.js
@@ -88,11 +88,37 @@ async function renew(sessionId) {
   }
 }
 
+async function expireById(sessionId) {
+  const expiredSessionObject = await runUpdatedQuery(sessionId);
+
+  return expiredSessionObject;
+
+  async function runUpdatedQuery(sessionId) {
+    const results = await database.query({
+      text: `
+      UPDATE
+        sessions
+      SET
+        expires_at = expires_at - interval '1 year',
+        updated_at = NOW()
+      WHERE
+        id = $1
+      RETURNING
+        *
+    ;`,
+      values: [sessionId],
+    });
+
+    return results.rows[0];
+  }
+}
+
 const session = {
   create,
   EXPIRATION_IN_MILLISECONDS,
   findOneValidByToken,
   renew,
+  expireById,
 };
 
 export default session;
